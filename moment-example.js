c56421c4b2b19b90d6093912f37e411193d6d179
Bob Taffet <bob.taffet@gmail.com>
2017-01-18 23:00:32 -0500

    show timestamp on ui

--- moment-example.js
+++ moment-example.js
@@ -1,6 +1,10 @@
-var moment = require('moment');
-var now = moment();
-var timestamp = 1444247486704;
-var timestampMoment = moment.utc(timestamp);
-console.log(timestampMoment.format('MMM Do YYYY, h:mm a'));
-console.log(timestampMoment.local().format('h:mm a'));
+ var moment = require('moment');
+// var now = moment();
+// var timestamp = 1444247486704;
+// var timestampMoment = moment.utc(timestamp);
+// console.log(timestampMoment.format('MMM Do YYYY, h:mm a'));
+// console.log(timestampMoment.local().format('h:mm a'));
+
+//var momentTimestamp = moment.utc(moment());
+var timestamp = moment.utc(moment()).local().format('h:mm : ');
+ console.log(timestamp);
